4188ed2a265b9bc9d73d2028dac9f2b77d189b03
Usman Ali <usman.ali@zaxiss.com>
2021-07-29 15:02:34 +0500

    Removed DICOM traces and implemented single file upload function

--- user/src/environments/environment.prod.ts
+++ user/src/environments/environment.prod.ts
@@ -7,26 +7,6 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
   production: true,
   apiUrl: 'https://app-dev.cb-portal.trilloapps.com',
-  TrilloAdminURL:'https://app-dev.cb-portal.trilloapps.com/cloud/UM',
-  AdminRoute:'https://app-dev.cb-portal.trilloapps.com/cloud/UM',
-  
-  httpOptions: 
-  {
-    headers: new HttpHeaders({
-                               'Content-Type': 'application/json',
-                               'Accept': 'application/json',
-                               'x-app-name': 'auth',
-                               'x-org-name': 'cloud',
-                             })
-  },
-  httpOptionsWithAccessToken: {
-    headers: new HttpHeaders({
-                               'Content-Type': 'application/json',
-                               'Accept': 'application/json',
-                               'x-app-name': 'main',
-                               'x-org-name': 'cloud',
-                             })
-  },
 };
 
 /*
